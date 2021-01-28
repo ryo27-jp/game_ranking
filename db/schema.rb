247c5ff9b19bccea1b92af2928e295b5268669f8
ryo27-jp <calvee.20@gmail.com>
2021-01-28 22:18:42 +0900

    gamesテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_141818) do
+ActiveRecord::Schema.define(version: 2021_01_28_130235) do
+
+  create_table "games", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "description", null: false
+    t.string "release_date", null: false
+    t.string "production", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
