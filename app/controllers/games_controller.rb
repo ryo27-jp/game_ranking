a37e77962eac05358c8f2402a7b77cc8ec380480
ryo27-jp <calvee.20@gmail.com>
2021-02-01 23:31:08 +0900

    games_controller削除機能

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,40 @@
 class GamesController < ApplicationController
+  # before_action :require_login
+
   def index
+    @games = Game.all
   end
 
   def show
   end
 
   def new
+    @game = Game.new
   end
 
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path notice: '作成しました'
+    else
+      flash.now[:danger] = '投稿に失敗しました。'
+      render :new
+    end
+  end
+
+
   def edit
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy!
+    redirect_to games_path, success: '投稿を削除しました。'
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :description, :release_date, :production)
   end
 end
