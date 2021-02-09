5ade972d883350b72c111d22d9056479f8bcc74c
ryo27-jp <calvee.20@gmail.com>
2021-02-09 21:33:00 +0900

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  Game.create!(
+    title: Faker::Game.title,
+    description: Faker::Game.platform,
+    release_date: Faker::Date.between(from: 2.days.ago, to: Date.today),
+    production: "制作会社#{n + 1}"
+  )
+end
