cf45db4b540dfb001a863071410c522bee6a8289
ryo27-jp <calvee.20@gmail.com>
2021-02-15 16:13:15 +0900

    bootstrapを使って少しview調整した

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_to games_path, notice: 'Login successful'
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
